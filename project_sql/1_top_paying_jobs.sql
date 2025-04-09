3b9393e327e4c73374121ce006bd7d549e5a1faf
schao021 <simcha918@gmail.com>
2025-04-09 14:08:27 -0700

    Added changes to problem 1

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -8,7 +8,8 @@ SELECT
     job_location,
     job_schedule_type,
     salary_year_avg,
-    job_posted_date
+    job_posted_date,
+    name AS company_name
 FROM job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE job_title_short = 'Data Analyst' AND 
