35a9da270048722c7570988142ba9f09b27236e2
schao021 <simcha918@gmail.com>
2025-04-09 14:08:04 -0700

    Finished problem 1

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,18 @@
+-- What are the top paying jobs for my role
+-- Find the top 10 highest paying remote data analyst jobs
+-- Focus on job postings with salaries
+-- Why is this important, offer insights into 
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE job_title_short = 'Data Analyst' AND 
+salary_year_avg IS NOT NULL AND
+job_location = 'Anywhere'
+ORDER BY salary_year_avg DESC
+LIMIT 10
